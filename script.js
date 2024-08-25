da6c97cfd1584e8ddb0b7a5fec9c7b110603287b
NicoLeon <hieubeax@gmail.com>
2024-08-25 15:57:48 +0700

    upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("donateButton").addEventListener("click", function () {
+  var donateImage = document.getElementById("donateImage");
+
+  donateImage.src = "./img/qr-code-15k.jpg";
+});
